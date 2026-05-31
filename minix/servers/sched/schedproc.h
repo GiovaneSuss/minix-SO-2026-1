dcd79d18104cbf3be98d5edd6f7cec9da34a41de
leticiavfr <leticia.franca@unifesp.br>
2026-05-31 11:44:27 -0300

    fix: corrige erros de compilação

--- minix/servers/sched/schedproc.h
+++ minix/servers/sched/schedproc.h
@@ -30,6 +30,7 @@ EXTERN struct schedproc {
 	unsigned priority;		/* the process' current priority */
 	unsigned time_slice;		/* this process's time slice */
 	unsigned cpu;		/* what CPU is the process running on */
+	unsigned int arrival_time;
 	bitchunk_t cpu_mask[BITMAP_CHUNKS(CONFIG_MAX_CPUS)]; /* what CPUs is the
 								process allowed
 								to run on */
@@ -38,5 +39,5 @@ EXTERN struct schedproc {
 /* Flag values */
 #define IN_USE		0x00001	/* set when 'schedproc' slot in use */
 
-unsigned int arrival_time;
+
 extern unsigned int fcfs_counter;
